9845ef9018cfb9c2639e2c9e73d4f5d07d36ec03
qiaoxu <qiaoxu@mtedu.com>
2019-05-21 10:24:12 +0800

    'typescript'

---
+++ src/components/hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+    //！是为了告诉typescript，msg必须传，{required:true}是告诉vue，msg必须传
+    @Prop({required: true}) private msg!: string;
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <!--<img alt="Vue logo" src="../assets/logo.png">-->
+        <!--<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
+        <hello msg="Welcome to Your Vue.js + TypeScript App"/>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import {Component, Vue} from 'vue-property-decorator';
+    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import hello from '@/components/hello.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    @Component({
+        components: {
+            HelloWorld,
+            hello,
+        },
+    })
+    export default class Home extends Vue {
+    }
 </script>
